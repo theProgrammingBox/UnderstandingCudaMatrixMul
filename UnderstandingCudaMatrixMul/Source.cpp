34a73c6dabd3f5e50d0f86c0345ca37f88c38b2e
theProgrammingBox <justinboxgea@gmail.com>
2022-10-29 11:58:43 -0700

    better vis on naive

--- UnderstandingCudaMatrixMul/Source.cpp
+++ UnderstandingCudaMatrixMul/Source.cpp
@@ -1,10 +1,21 @@
 #include "naiveGPUVis.h"
+#include "tileGPUVis.h"
 
 int main()
 {
-	NaiveGPU niaveGPU;
-	if (niaveGPU.Construct(1000, 1000, 1, 1))
-		niaveGPU.Start();
+	int option = 0;
+	if (option == 0)
+	{
+		NaiveGPU naive;
+		if (naive.Construct(800, 600, 1, 1))
+			naive.Start();
+	}
+	else
+	{
+		TileGPU tile;
+		if (tile.Construct(800, 600, 1, 1))
+			tile.Start();
+	}
 
 	return 0;
 }
--- UnderstandingCudaMatrixMul/tileGPUVis.h
+++ UnderstandingCudaMatrixMul/tileGPUVis.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "header.h"
 
-class NaiveGPU : public olc::PixelGameEngine
+class TileGPU : public olc::PixelGameEngine
 {
 private:
 	int step = 0;
@@ -36,18 +36,21 @@ private:
 	uint32_t outputFeaturesCeil = outputFeaturesCeilBlocks * BLOCK_SIZE;
 
 	vf2d inputMatrixStartPos = vf2d(0, 0);
-	vf2d weightMatrixStartPos = inputMatrixStartPos + vf2d(scale * inputFeatures, scale * inputEntries);
+	vf2d weightMatrixStartPos = inputMatrixStartPos + vf2d(scale * inputFeatures, scale * inputEntriesCeil);
 	vf2d outputMatrixStartPos = inputMatrixStartPos + vf2d(scale * inputFeatures, 0);
-	vf2d outputBlockMatrixStartPos = outputMatrixStartPos + vf2d(scale * outputFeatures + 10, 0);
+	vf2d inputBlockMatrixStartPos = outputMatrixStartPos + vf2d(scale * outputFeaturesCeil, 0);
+	vf2d weightBlockMatrixStartPos = inputBlockMatrixStartPos + vf2d(scale * BLOCK_SIZE, 0);
 	vf2d blockSize = vf2d(BLOCK_SIZE * scale, BLOCK_SIZE * scale);
 	vf2d prevBlockPos = vf2d(-1000, -1000);
 	vf2d prevThreadPos = vf2d(-1000, -1000);
-	vf2d previnputPos = vf2d(-1000, -1000);
+	vf2d prevInputPos = vf2d(-1000, -1000);
 	vf2d prevWeightPos = vf2d(-1000, -1000);
 	vf2d prevOutputPos = vf2d(-1000, -1000);
+	vf2d prevInputBlockPos = vf2d(-1000, -1000);
+	vf2d prevWeightBlockPos = vf2d(-1000, -1000);
 
 public:
-	NaiveGPU()
+	TileGPU()
 	{
 		sAppName = "Visualize naive GPU";
 	}
@@ -61,16 +64,18 @@ public:
 		FillZero(outputMatrix, outputMatrixBytes);
 		MatrixMulCPU(inputMatrix, weightMatrix, outputMatrix, inputEntries, inputFeatures, outputFeatures);
 		PrintMatrix(outputMatrix, inputEntries, outputFeatures);
-		
+
 		DrawRect(inputMatrixStartPos, vf2d(inputFeatures, inputEntries) * scale, GREEN);
 		DrawRect(weightMatrixStartPos, vf2d(outputFeatures, inputFeatures) * scale, BLUE);
 		DrawRect(outputMatrixStartPos, vf2d(outputFeatures, inputEntries) * scale, RED);
-		
+		DrawRect(inputBlockMatrixStartPos, blockSize, GREEN);
+		DrawRect(weightBlockMatrixStartPos, blockSize, BLUE);
+
 		for (uint32_t blocky = 0; blocky < inputEntriesCeilBlocks; blocky++)
 		{
 			for (uint32_t blockx = 0; blockx < outputFeaturesCeilBlocks; blockx++)
 			{
-				DrawRect(outputBlockMatrixStartPos + vf2d(blockx, blocky) * blockSize, blockSize, GREY);
+				DrawRect(outputMatrixStartPos + vf2d(blockx, blocky) * blockSize, blockSize, DARK_GREY);
 			}
 		}
 
@@ -90,19 +95,27 @@ public:
 			}
 		}
 
-		for (int i = 0; i < outputFeatures; i++)
-		{
-			for (int j = 0; j < inputEntries; j++)
-			{
-				DrawCircle(outputMatrixStartPos + vf2d(hscale + i * scale, hscale + j * scale), hscalem);
-			}
-		}
-
 		for (int i = 0; i < inputEntriesCeil; i++)
 		{
 			for (int j = 0; j < outputFeaturesCeil; j++)
 			{
-				DrawCircle(outputBlockMatrixStartPos + vf2d(hscale + j * scale, hscale + i * scale), hscalem);
+				DrawCircle(outputMatrixStartPos + vf2d(hscale + j * scale, hscale + i * scale), hscalem);
+			}
+		}
+
+		for (int i = 0; i < BLOCK_SIZE; i++)
+		{
+			for (int j = 0; j < BLOCK_SIZE; j++)
+			{
+				DrawCircle(inputBlockMatrixStartPos + vf2d(hscale + j * scale, hscale + i * scale), hscalem);
+			}
+		}
+
+		for (int i = 0; i < BLOCK_SIZE; i++)
+		{
+			for (int j = 0; j < BLOCK_SIZE; j++)
+			{
+				DrawCircle(weightBlockMatrixStartPos + vf2d(hscale + j * scale, hscale + i * scale), hscalem);
 			}
 		}
 
@@ -113,7 +126,7 @@ public:
 	{
 		if (GetKey(olc::Key::SPACE).bHeld)
 		{
-			const float timef = 0.001f;
+			const float timef = 0.01f;
 			if (GetKey(olc::Key::UP).bHeld)
 			{
 				keyD += fElapsedTime;
@@ -147,13 +160,15 @@ public:
 		vf2d outputDPos;
 		vf2d inputDPos;
 		vf2d weightDPos;
+		vf2d inputBlockDPos;
+		vf2d weightBlockDPos;
 		int step2 = 0;
 		for (uint32_t blockx = 0; blockx < outputFeaturesCeilBlocks; blockx++)
 		{
 			for (uint32_t blocky = 0; blocky < inputEntriesCeilBlocks; blocky++)
 			{
 				blockDMPos = vf2d(blockx, blocky) * blockSize;
-				blockDPos = outputBlockMatrixStartPos + blockDMPos;
+				blockDPos = outputMatrixStartPos + blockDMPos;
 
 				for (uint32_t threadx = 0; threadx < BLOCK_SIZE; threadx++)
 				{
@@ -161,17 +176,21 @@ public:
 					{
 						if (blockx * BLOCK_SIZE + threadx < outputFeatures && blocky * BLOCK_SIZE + thready < inputEntries)
 						{
-							threadDPos = blockDPos + vf2d(threadx * scale + hscale, thready * scale + hscale);
+							/*threadDPos = blockDPos + vf2d(threadx * scale + hscale, thready * scale + hscale);
 							outputDPos = outputMatrixStartPos + blockDMPos + vf2d(threadx * scale + hscale, thready * scale + hscale);
 
 							for (uint32_t k = 0; k < inputFeatures; k++)
 							{
 								inputDPos = inputMatrixStartPos + vf2d(k * scale + hscale, thready * scale + hscale + blockDMPos.y);
+
 								weightDPos = weightMatrixStartPos + vf2d(threadx * scale + hscale + blockDMPos.x, k * scale + hscale);
 
 								step2++;
 								if (step2 > step) break;
-							}
+							}*/
+							inputDPos = inputMatrixStartPos + vf2d(threadx * scale + hscale, thready * scale + hscale + blockDMPos.y);
+							inputBlockDPos = inputBlockMatrixStartPos + vf2d(threadx * scale + hscale, thready * scale + hscale);
+							step2++;
 						}
 						if (step2 > step) break;
 					}
@@ -182,21 +201,27 @@ public:
 			if (step2 > step) break;
 		}
 		if (step == step2) step--;
-		DrawRect(prevBlockPos, blockSize, GREY);
+		DrawRect(prevBlockPos, blockSize, DARK_GREY);
 		DrawRect(blockDPos, blockSize, YELLOW);
 		DrawCircle(prevThreadPos, hscalem);
 		DrawCircle(threadDPos, hscalem, DARK_GREY);
 		DrawCircle(prevOutputPos, hscalem);
 		DrawCircle(outputDPos, hscalem, DARK_GREY);
-		DrawCircle(previnputPos, hscalem);
+		DrawCircle(prevInputPos, hscalem);
 		DrawCircle(inputDPos, hscalem, DARK_GREY);
 		DrawCircle(prevWeightPos, hscalem);
 		DrawCircle(weightDPos, hscalem, DARK_GREY);
+		DrawCircle(prevInputBlockPos, hscalem);
+		DrawCircle(inputBlockDPos, hscalem, DARK_GREY);
+		DrawCircle(prevWeightBlockPos, hscalem);
+		DrawCircle(weightBlockDPos, hscalem, DARK_GREY);
 		prevBlockPos = blockDPos;
 		prevThreadPos = threadDPos;
 		prevOutputPos = outputDPos;
-		previnputPos = inputDPos;
+		prevInputPos = inputDPos;
 		prevWeightPos = weightDPos;
+		prevInputBlockPos = inputBlockDPos;
+		prevWeightBlockPos = weightBlockDPos;
 
 		return true;
 	}
@@ -220,7 +245,7 @@ public:
 //
 //int main()
 //{
-//	NaiveGPU demo;
+//	TileGPU demo;
 //	float* gpuInput;
 //	float* gpuWeight;
 //	float* gpuOutput;
